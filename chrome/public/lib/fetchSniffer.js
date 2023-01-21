41ced0f50719237e4f23ca3911713823a59a8df2
Kevin Luxa <k.luxa@unsw.edu.au>
2023-01-21 16:05:51 +1100

    More cleanup

--- chrome/public/lib/fetchSniffer.js
+++ chrome/public/lib/fetchSniffer.js
@@ -1,33 +1,33 @@
 // Taken from https://blog.logrocket.com/intercepting-javascript-fetch-api-requests-responses/
 
 (function() {
-    const originalFetch = window.fetch;
-    window.fetch = async (...args) => {
-        let [resource, config] = args;
-        const response = await originalFetch(resource, config);
-        
-        // response interceptor
-        const json = () => response.clone().json().then((data) => {
-            const url = resource.url;
-            if (url.match(/^https:\/\/edstem\.org\/api\/user$/)) {
-                document.dispatchEvent(new CustomEvent('fetchData', {
-                    detail: {
-                        type: 'user-data',
-                        data: data,
-                    },
-                }));
-            } else if (url.match(/^https:\/\/edstem\.org\/api\/threads\/.*\?.*/)) {
-                document.dispatchEvent(new CustomEvent('fetchData', {
-                    detail: {
-                        type: 'thread-data',
-                        data: data,
-                    },
-                }));
-            }
-            return data;
-        });
+  const originalFetch = window.fetch;
+  window.fetch = async (...args) => {
+    let [resource, config] = args;
+    const response = await originalFetch(resource, config);
 
-        response.json = json;
-        return response;
-    }
+    // response interceptor
+    const json = () => response.clone().json().then((data) => {
+      const url = resource.url;
+      if (url.match(/^https:\/\/edstem\.org\/api\/user$/)) {
+        document.dispatchEvent(new CustomEvent('fetchData', {
+          detail: {
+            type: 'user-data',
+            data: data,
+          },
+        }));
+      } else if (url.match(/^https:\/\/edstem\.org\/api\/threads\/.*\?.*/)) {
+        document.dispatchEvent(new CustomEvent('fetchData', {
+          detail: {
+            type: 'thread-data',
+            data: data,
+          },
+        }));
+      }
+      return data;
+    });
+
+    response.json = json;
+    return response;
+  }
 })();
